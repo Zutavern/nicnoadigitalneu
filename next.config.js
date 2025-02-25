85f61f953eb32442cb3dc3641ba9471add93fa5b
Daniel <zutavern@gmail.com>
2025-02-25 17:30:26 +0100

    chore: Ignore ESLint during build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['yqzunbubsqmsfsayneeh.supabase.co', 'ui-avatars.com'],
   },
+  eslint: {
+    // Warnung: Dies ist nur für den Build-Prozess. In der Entwicklung sollten die ESLint-Regeln aktiv bleiben.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig 
