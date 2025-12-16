337086a951f9121092d03ade66fb052722f1e0e7
Daniel <zutavern@gmail.com>
2025-12-16 23:06:59 +0100

    fix: Null-Check für currentInterval auf Preisseite

--- src/app/preise/page.tsx
+++ src/app/preise/page.tsx
@@ -387,7 +387,7 @@ export default function PricingPage() {
               ))}
             </div>
 
-            {currentInterval.discount > 0 && (
+            {currentInterval && currentInterval.discount > 0 && (
               <motion.div 
                 initial={{ opacity: 0, y: 10 }}
                 animate={{ opacity: 1, y: 0 }}
